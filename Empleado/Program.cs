6ee58ae3c5f5769bc314b076e79b55f959e75737
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-09-12 15:54:59 -0600

    Agregar archivos de proyecto.

---
+++ Empleado/Program.cs
@@ -0,0 +1,26 @@
+﻿Empleado empleado = new Empleado("Ana", "Programadora", 3500m);
+empleado.AumentarSalario(10);
+
+public class Empleado
+{
+    public string Nombre { get; set; }
+    public string Puesto { get; set; }
+    private decimal _salario;
+
+    public Empleado(string nombre, string puesto, decimal salario)
+    {
+        Nombre = nombre;
+        Puesto = puesto;
+        _salario = salario;
+    }
+    // Método para aumentar el salario
+    public void AumentarSalario(decimal porcentaje)
+    {
+        if(porcentaje > 0)
+        {
+            _salario += _salario * porcentaje / 100;
+            Console.WriteLine($"El salario de {Nombre} ha sido aumentado en " +
+                $"{porcentaje}%. Nuevo salario: {_salario:C}");
+        }
+    }
+}
